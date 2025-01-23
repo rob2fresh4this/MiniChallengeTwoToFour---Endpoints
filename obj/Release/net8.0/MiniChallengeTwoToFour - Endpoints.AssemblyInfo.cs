f969d479c1e97d655ffc695860b0c532b8b67aef
rob2fresh4this <grijrob07@gmail.com>
2025-01-23 15:24:13 -0800

    Add configuration files for logging and runtime settings

--- obj/Release/net8.0/MiniChallengeTwoToFour - Endpoints.AssemblyInfo.cs
+++ obj/Release/net8.0/MiniChallengeTwoToFour - Endpoints.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniChallengeTwoToFour - Endpoints")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+755b6523534731346c6a5de714e5b55a98d20567")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniChallengeTwoToFour - Endpoints")]
